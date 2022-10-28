721009ab5815b9f201d8281ece82260ebc34d2ba
izrael707 <dkingisreal@gmail.com>
2022-10-28 10:33:56 +0100

    footer component done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return ( <div></div> );
+  return ( 
+    <div className="d-flex flex-column flex-md-row p-3 border-gray">
+      <img src="zuri-logo.svg" alt="" width='120px' height='30px' />
+      <span className="text-muted flex-grow-1 text-md-center">HNG Internship 9 Frontend Task</span>
+      <img src="ingress-logo.svg" alt="" width='88px' height='30px' />
+    </div>
+  );
 }
  
 export default Footer;
