de4ad6031f6ad70e7f3fa77d13250fd541fc2813
izrael707 <dkingisreal@gmail.com>
2022-11-05 22:50:08 +0100

    feat: finalising the contact form component

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,16 @@
 import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
 
 const Contact = () => {
   return (
-    <div container-md>
-      <span className="display-5 fw-bold pt-3 px-2 pb-1 d-block">Contact Me</span>
-      <span className="h6 fw-normal p-2 d-block">Hi there, contact me to ask me about anything you have in mind.</span>
-      <ContactForm />
-    </div>
+    <>
+      <div className="container-md w-50 p-5">
+        <span className="h2 fw-semibold pt-3 px-2 pb-1 d-block">Contact Me</span>
+        <span className="h6 fw-normal p-2 d-block">Hi there, contact me to ask me about anything you have in mind.</span>
+        <ContactForm />
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
 export default function ContactForm() {
   return (
-    <div className="pt-4 px-2">
-      <div className="row g-2">
+    <form className="pt-4 px-2 needs-validation" noValidate>
+      <div className="row g-3">
         <div className="col-12 col-md-6">
           <label for="first_name" className="form-label fw-semibold">First Name</label>
           <input className="form-control w-100" id="first_name" type="text" placeholder="Enter your first name" />
@@ -11,18 +11,24 @@ export default function ContactForm() {
           <input className="form-control" id="last_name" type="text" placeholder="Enter your last name" />
         </div>
       </div>
-      <div className="pt-2">
+      <div className="pt-3">
         <label for="email" className="form-label fw-semibold">Email</label>
         <input className="form-control" id="email" type="email" placeholder="israel707@gmail.com" />
       </div>
-      <div className="pt-2">
+      <div className="pt-3">
         <label for="message" className="form-label fw-semibold">Message</label>
-        <textarea rows="3" id="message" className="form-control" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+        <textarea rows="3" id="message" className="form-control" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+        <div className="invalid-feedback">
+          Please enter a message in the textarea
+        </div>
       </div>
-      <div className="form-check pt-2">
+      <div className="form-check pt-3">
         <input type="checkbox" class="form-check-input" id="form_check" />
         <label className="form-check-label fw-normal" for="form_check">You agree to providing your data to Israel707 who may contact you</label>
       </div>
-    </div>
+      <div class="pt-3">
+        <button class="btn btn-primary w-100" type="submit" id="btn__submit">Send message</button>
+      </div>
+    </form>
   )
 }
