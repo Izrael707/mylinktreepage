e7f7f49ad0cd3d8b20909043733394b50175057b
izrael707 <dkingisreal@gmail.com>
2022-11-11 20:27:29 +0100

    fix: mobile responsiveness

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 const Contact = () => {
   return (
     <>
-      <div className="container-md w-50 p-5 mb-5">
+      <div className="container-md p-md-5 w-75 mb-5">
         <span className="h2 fw-semibold pt-3 px-2 pb-1 d-block">Contact Me</span>
         <span className="h6 fw-normal p-2 d-block">Hi there, contact me to ask me about anything you have in mind.</span>
         <ContactForm />
